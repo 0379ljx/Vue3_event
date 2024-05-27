1930b8e026dafffb51f7c4b468f4c67b44190798
ljx <lk2236014477@163.com>
2024-05-27 16:12:11 +0800

    t t

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import zh from 'element-plus/es/locale/lang/zh-cn.mjs'
+</script>
+
+<template>
+  <div>
+    <!-- 国际化处理 -->
+    <el-config-provider :locale="zh">
+      <!-- App.vue只需要留一个路由出口 router-view即可 -->
+      <router-view></router-view>
+    </el-config-provider>
+  </div>
+</template>
+
+<style scoped></style>
